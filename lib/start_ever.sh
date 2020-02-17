17c35891f49dbf831b3ccbdbe11ae6f502ef4bd0
Shucream <shuya.uchida@gmail.com>
2020-02-18 02:27:13 +0900

    auto commit

--- lib/start_ever.sh
+++ lib/start_ever.sh
@@ -5,8 +5,6 @@ DIR=$(dirname $(cd $(dirname $0);pwd))
 PID_FILE="$DIR"
 LOG_FILE="$DIR/etc/log"
 
-trap 'echo "" > etc/pid' 2
-
 TARGET_DIR=$1
 INTERVAL_TIME=$2
 TARGET_BRANCH=$3
@@ -24,14 +22,15 @@ while [ "1" = "1" ];
 do
 CURRENT_BRANCH="$(git symbolic-ref --short HEAD)"
 if [ "$CURRENT_BRANCH" = "$TARGET_BRANCH" ]; then
-    "git add ."
-    "git commit -m 'auto commit'"
-    "git push origin $TARGET_BRANCH"
-    "git pull origin $TARGET_BRANCH"
-    echo "Success"
+    git add . && \
+        git commit -m 'auto commit' && \
+        git push origin $TARGET_BRANCH && \
+        git pull origin $TARGET_BRANCH && \
+        echo "Success"
 else
     echo "Fail by different branch $CURRENT_BRANCH"
 fi
+echo "sleep"
 sleep $INTERVAL_TIME
 done
 
